3af9d872c7d0dd30aef8fb2728585c6e230c6215
delphinhugi <delphinhugi@gmail.com>
2017-01-25 09:45:56 +0000

    Added second function

--- forritun.py
+++ forritun.py
@@ -1,6 +1,5 @@
 #delphin
 
-
 nr_01 = float(input('Please input a number: '))
 nr_02 = float(input('Please input another number: '))
 
@@ -9,3 +8,11 @@ formula_02 = (nr_01 * nr_02)
 
 print("These two number put together:", formula_01)
 print("These two numbers multiplied:", formula_02)
+
+first_name = input("Please input your first name: ")
+
+last_name = input("Please input your last name: ")
+
+print("Your first name:", first_name)
+print("Your last name:", last_name)
+print("Hello", first_name, last_name)
